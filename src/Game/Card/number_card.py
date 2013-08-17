7f5f2cd99feead4ff361e57a60dbb777950f1234
Chris Loew <cloew123@gmail.com>
2013-08-17 11:12:45 -0600

    Added Number Card

---
+++ src/Game/Card/number_card.py
@@ -0,0 +1,12 @@
+
+class NumberCard:
+    """ Represents a number card """
+    
+    def __init__(self, number, color):
+        """ Initialize the Number Card """
+        self.number = number
+        self.color = color
+        
+    def __repr__(self):
+        """ Return the String representation """
+        return self.number
