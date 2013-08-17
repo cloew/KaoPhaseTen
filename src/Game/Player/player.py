a7645b4f7c221c48eaafeec6b8fcc26cdee4c9bd
Chris Loew <cloew123@gmail.com>
2013-08-17 13:19:05 -0600

    Added stubbed out Player

---
+++ src/Game/Player/player.py
@@ -0,0 +1,10 @@
+
+class Player:
+    """ Represents a Player """
+    
+    def __init__(self, phaseList):
+        """ Initialize the Player """
+        self.hand = []
+        self.phaseList = phaseList
+        self.phase = phaseList.getStartPhase()
+    
--- src/Game/Test/suite.py
+++ src/Game/Test/suite.py
@@ -1,10 +1,12 @@
 import unittest
 
+from Game.Player.Test.suite import suite as player_suite
 from Game.Phase.Test.suite import suite as phase_suite
 from Game.Card.Test.suite import suite as card_suite
 from Game.Test.round_test import suite as round_suite
 
 suites = [round_suite,
           card_suite,
-          phase_suite]
+          phase_suite,
+          player_suite]
 suite = unittest.TestSuite(suites)
