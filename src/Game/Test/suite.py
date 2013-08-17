21cc323df1ab35aa5ddcc986d615bdfeec89d88d
Chris Loew <cloew123@gmail.com>
2013-08-17 11:13:05 -0600

    Added Card Test suite to parent suite

--- src/Game/Test/suite.py
+++ src/Game/Test/suite.py
@@ -1,6 +1,8 @@
 import unittest
 
+from src.Game.Card.Test.suite import suite as card_suite
 from Game.Test.round_test import suite as round_suite
 
-suites = [round_suite]
+suites = [round_suite,
+          card_suite]
 suite = unittest.TestSuite(suites)
