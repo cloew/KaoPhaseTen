b5ce13c28aaca9c65abe52fc96eaf5e108e5f26f
Chris Loew <cloew123@gmail.com>
2013-08-17 11:34:20 -0600

    Added stubbed out Phase class

---
+++ src/Game/Phase/phase.py
@@ -0,0 +1,7 @@
+
+class Phase:
+    """ Represents a Phase """
+    
+    def __init__(self, matches):
+        """ Initialize the Phase with the matches that must be made """
+        self.matches = matches
--- src/Game/Test/suite.py
+++ src/Game/Test/suite.py
@@ -1,8 +1,10 @@
 import unittest
 
+from src.Game.Phase.Test.suite import suite as phase_suite
 from src.Game.Card.Test.suite import suite as card_suite
 from Game.Test.round_test import suite as round_suite
 
 suites = [round_suite,
-          card_suite]
+          card_suite,
+          phase_suite]
 suite = unittest.TestSuite(suites)
