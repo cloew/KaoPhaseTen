2bcd3179132526012e6f7adf9f348fae2abf3bef
Chris Loew <cloew123@gmail.com>
2013-08-20 18:05:53 -0600

    Added class to represent a match pile

---
+++ src/Game/match_pile.py
@@ -0,0 +1,8 @@
+
+class MatchPile:
+    """ Represents a Match Pile """
+    
+    def __init__(self, match, cards):
+        """ Initialize the Match Pile """
+        self.match = match
+        self.cards = cards
