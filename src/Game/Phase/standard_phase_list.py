cf3b3c3a85f760f6c694940e1cb4840a78061ba1
Chris Loew <cloew123@gmail.com>
2013-08-27 13:26:34 -0600

    Added Run Phase

--- src/Game/Phase/standard_phase_list.py
+++ src/Game/Phase/standard_phase_list.py
@@ -1,6 +1,7 @@
 from Game.Phase.phase import Phase
 from Game.Phase.phase_list import PhaseList
 from Game.Phase.Match.number_set import NumberSet
+from Game.Phase.Match.run import Run
 
 class StandardPhaseList(PhaseList):
     """ Represents the standard Phase List """
@@ -12,7 +13,8 @@ class StandardPhaseList(PhaseList):
     def buildPhases(self):
         """ Builds and returns the standard phases in a list """
         phase = Phase([NumberSet(3), NumberSet(3)])
+        phase2 = Phase([NumberSet(3), Run(4)])
         phase9 = Phase([NumberSet(5), NumberSet(2)])
-        return [phase, phase9]
+        return [phase, phase2, phase9]
         
 StandardPhaseList = StandardPhaseList()
