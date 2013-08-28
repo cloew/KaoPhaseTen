0c9766076ec13a33cef6390aa0c132f4bdfbc414
Chris Loew <cloew123@gmail.com>
2013-08-27 22:02:13 -0600

    Added Wild Cards to the deck

--- src/Game/game_deck.py
+++ src/Game/game_deck.py
@@ -1,5 +1,6 @@
 from Game.globals import colors
 from Game.Card.number_card import NumberCard
+from Game.Card.wild_card import WildCard
 
 from kao_deck.deck import Deck
 from kao_deck.deck_initializer import DeckInitializer
@@ -21,5 +22,8 @@ class GameDeck(Deck):
             for color in colors:
                 card = NumberCard(i, color)
                 deckInitializer.addSameItem(card, 2)
+                
+        wildCard = WildCard()
+        deckInitializer.addSameItem(wildCard, 8)
                 
         return deckInitializer
