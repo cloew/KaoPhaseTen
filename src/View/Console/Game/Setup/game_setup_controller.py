7853c8809dd6c4a236a4008d8f3455c8450f66d8
Chris Loew <cloew123@gmail.com>
2013-08-27 18:28:46 -0600

    Added controller to pick the number of players

---
+++ src/View/Console/Game/Setup/game_setup_controller.py
@@ -0,0 +1,20 @@
+from View.Console.Game.Setup.game_setup_screen import GameSetupScreen
+
+from kao_gui.console.console_controller import ConsoleController
+
+class GameSetupController(ConsoleController):
+    """ Controller for Game Setup """
+    
+    def __init__(self):
+        """ Initialize the Game Setup Controller """
+        screen = GameSetupScreen()
+        ConsoleController.__init__(self, screen, commands={'1':self.setPlayerCount,
+                                                           '2':self.setPlayerCount,
+                                                           '3':self.setPlayerCount,
+                                                           '4':self.setPlayerCount,
+                                                           '5':self.setPlayerCount,
+                                                           '6':self.setPlayerCount})
+        
+    def setPlayerCount(self, event):
+        """ Set the player Count """
+        self.playerCount = int(event)
---
+++ src/View/Console/Game/Setup/game_setup_screen.py
@@ -0,0 +1,11 @@
+from kao_gui.console.console_widget import ConsoleWidget
+
+class GameSetupScreen(ConsoleWidget):
+    """ Represents the view for the Game Setup Screen """
+    
+    def __init__(self):
+        """ Initialize the view """
+        
+    def draw(self):
+        """ Draw the Widget """
+        print "Select number of players (1-6) to play with.\r"
