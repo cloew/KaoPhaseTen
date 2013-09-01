2d7574a0c07fb3adcafb8b04e95543e5e85807b0
Chris Loew <cloew123@gmail.com>
2013-09-01 16:44:46 -0600

    Can now choose the player to skip

--- src/View/Console/Game/Player/skip_controller.py
+++ src/View/Console/Game/Player/skip_controller.py
@@ -1,5 +1,6 @@
 from View.Console.Game.Player.skip_screen import SkipScreen
 
+from kao_console.ascii import KAO_UP, KAO_DOWN, ENDL
 from kao_gui.console.console_controller import ConsoleController
 
 class SkipController(ConsoleController):
@@ -9,16 +10,31 @@ class SkipController(ConsoleController):
         """ Initialize the *** Controller """
         self.players = list(players)
         self.players.remove(player)
+        self.players = [player for player in self.players if not player.skipped]
         
         screen = SkipScreen(self.players)
-        ConsoleController.__init__(self, screen)
+        ConsoleController.__init__(self, screen, commands={KAO_UP:self.previous,
+                                                           KAO_DOWN:self.next,
+                                                           ENDL:self.skipPlayer})
         
         if len(self.players) == 1:
             self.players[0].skipped = True
-            
-    
         
     def performGameCycle(self):
         """ Perform a Game Cycle Event """
         if len(self.players) < 2:
             self.stopRunning()
+            
+    def skipPlayer(self, event):
+        """ Skip the selected Player """
+        if self.screen.selected is not None:
+            self.screen.selected.skipped = True
+            self.stopRunning()
+        
+    def next(self, event):
+        """ Select the next player """
+        self.screen.selectNextPlayer()
+        
+    def previous(self, event):
+        """ Select the previous player """
+        self.screen.selectPreviousPlayer()
--- src/View/Console/Game/Player/skip_screen.py
+++ src/View/Console/Game/Player/skip_screen.py
@@ -6,7 +6,30 @@ class SkipScreen(ConsoleWidget):
     def __init__(self, players):
         """ Initialize the view """
         self.players = players
+        if len(self.players) > 0:
+            self.selected = players[0]
+        else:
+            self.selected = None
         
     def draw(self):
         """ Draw the Widget """
-        print self.players
+        for player in self.players:
+            if player is self.selected:
+                header = "-->"
+            else:
+                header = "   "
+            print "{0} {1}\r".format(header, player)
+            
+    def selectNextPlayer(self):
+        """ Select the Next Player, if possible """
+        index = self.players.index(self.selected)
+        index += 1
+        if index < len(self.players):
+            self.selected = self.players[index]
+            
+    def selectPreviousPlayer(self):
+        """ Select the Previous Player, if possible """
+        index = self.players.index(self.selected)
+        index -= 1
+        if index >= 0:
+            self.selected = self.players[index]
